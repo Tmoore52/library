b30626197c32fb20f95798531907a2bc9110ec70
Tyler Moore <tylermoore5252@gmail.com>
2023-03-19 16:47:09 -0400

    Modal done

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 let myLibrary = [];
-let add_book = document.getElementById("add_book")
+let add_book = document.getElementById("add_book");
+let modal = document.getElementById("new_book_modal");
+let remove_modal = document.getElementById("remove_modal");
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  
-    this.info = function() {
-      return title + " by " + author + ", " + pages + ", " + read
-    }
+add_book.onclick = function () {
+  modal.style.display = "flex";
+};
+
+remove_modal.onclick = function () {
+  modal.style.display = "none";
+};
+
+function Book(title, author, genre, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return title + " by " + author + ", " + pages + ", " + read;
+  };
 }
 function addBookToLibrary() {}
 
-
-
 // theHobbit = new Book("The Hobbit", "J.R.R. Tolkien","295 Pages", "have read")
-
